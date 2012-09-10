683b6230d158bb521a46c20200f5e38bf9e31f5d
Kashyap Bhatt <kpbhatt@mail.fresnostate.edu>
2012-09-09 18:36:23 -0700

    First commit with Device Setup with Mongoid and user registration and login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpenseManager::Application.config.secret_token = 'cfd9579f5179d00d7324b0de279d79dc6d1f485d57522600ec5a57e16daa9617075364ee0149a828b4659b92386c0943aafb122540dc1097911a2c31232f225e'
