370b76ff323db6eff06707561eceaa018a8574a7
Kashyap Bhatt <kpbhatt@mail.fresnostate.edu>
2012-09-21 20:32:51 -0700

    local app working and heroku working but without layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 	before_filter :authenticate_user!
 
   def show
+  	params[:data] = params[:id]
   	@user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 ExpenseManager::Application.routes.draw do
-  #get "users/show"
-
-  get "home/index"
-
+  # get "users/show"
+	root :to => 'home#index'
   devise_for :users
   resources :users, :only => :show
-
+	
+	get "home/index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,7 +54,7 @@ ExpenseManager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  # root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
