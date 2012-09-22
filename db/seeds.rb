7707547e8c3b8256d1236150d72b6173ab1356c2
Kashyap Bhatt <kpbhatt@mail.fresnostate.edu>
2012-09-21 19:24:16 -0700

    heroku mongolab configurations

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 puts 'EMPTY THE MONGODB DATABASE'
 #Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
-#Mongoid.purge!
+Mongoid.purge!
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
